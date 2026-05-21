3513f69dac27deeaea0ecb4874242358e1edbe59
Leonardo Martinez <lfmaph71@gmail.com>
2026-05-21 16:59:57 -0500

    Servicio calculador de EVM con sus indicadores e interpretacion de  CPI/SPI

---
+++ Backend/EVM.API/Models/Enums/EvmStatus.cs
@@ -0,0 +1,11 @@
+namespace EVM.API.Models.Enums;
+
+public static class EvmInterpretation
+{
+    public const string UnderBudget = "Bajo presupuesto";
+    public const string OverBudget = "Sobre presupuesto";
+    public const string AheadSchedule = "Adelantado";
+    public const string BehindSchedule = "Atrasado";
+    public const string OnBudget = "En presupuesto";
+    public const string OnSchedule = "En cronograma";
+}
